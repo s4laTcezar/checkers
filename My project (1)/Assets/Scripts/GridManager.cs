c1a14711b3468ac46762b15d82ef8b22d28b2172
s4laTcezar <salatfromrome@gmail.com>
2023-09-10 19:53:06 +0200

    Board
    whiteboard

---
+++ My project (1)/Assets/Scripts/GridManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int _width, _height;
+
+    [SerializeField] private Tile _tilePrefab;
+
+    //[SerializeField] private Transform _mainCam;
+
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    void GenerateGrid()
+    {
+        for (int y = 0; y < _height; y++)
+        {
+            for (int x = 0; x < _width; x++)
+            {
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+
+                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+                spawnedTile.Init(isOffset);
+            }
+        }
+    }
+
+    
+}
